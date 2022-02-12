c9172e12e6d448e03f540d022475a029858ba31f
Rinat Valiullov <4rontender@gmail.com>
2022-02-12 13:03:21 +0500

    add "else" branch

--- foreach/source/app.d
+++ foreach/source/app.d
@@ -4,10 +4,11 @@ import std.stdio;
 
 void main(string[] args)
 {
-	foreach (n; 1 .. 100)
-	{
-		if (n % 2 == 0)
-			writeln(n);
-	}
-
+  foreach (n; 1 .. 100)
+  {
+    if (n % 2 == 0)
+      writeln("Even number:", n);
+    else
+      writeln("Nope");
+  }
 }
